e4c2baee1d66b93ba562b2ac4fcff88ad9a91422
William Ireland <wireland@yoda.mayo.caltech.edu>
2015-04-10 12:00:15 -0700

    generic edits

--- MCMC/generic_energy_matrix_mscsvariedbin.py
+++ MCMC/generic_energy_matrix_mscsvariedbin.py
@@ -4,12 +4,14 @@ sys.path.append('/home/wireland/sortseq/utils/')
 import scipy as sp
 import pymc
 import scipy.ndimage
+import glob
 import MCMC_utils_mscs
 import MCMC_utils
 import ConfigParser
 import mscscfg
 import os
 import numpy as np
+import csv
 
 import readuniqueseqssingleend
 numbins = 4
